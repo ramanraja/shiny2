8ca91c949247084ff4ee6dc8d1737d8164c217ac
ramanraja <rajaraman.v@gmail.com>
2014-06-21 19:31:43 +0530

    My shiny app on Rstudio server
    https://ramanraja.shinyapps.io/shiny2/

---
+++ run.R
@@ -0,0 +1,12 @@
+# http://www.rstudio.com/shiny/
+# https://ramanraja.shinyapps.io/shiny2/
+
+# To deploy it on server:
+library(shinyapps)
+deployApp()
+
+
+#To run it locally:
+library(shiny)
+runApp()
+
